dc83d0c080cc533870a8d328898dca9654d8426c
cjfei18 <53482192+cjfei18@users.noreply.github.com>
2024-04-16 21:57:19 +0800

    Add files via upload

---
+++ single_variant_test.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#SBATCH -J depression_single
+#SBATCH -p DCU
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --ntasks-per-node=1
+#SBATCH -o %j.out
+#SBATCH -e %j.err
+
+module load miniconda3/RSAIGE
+
+for i in {1..22};
+do
+  Rscript /public/home/zyli/UKB_WES_3rd/SAIGE_Rfile/step1_fitNULLGLMM.R \
+    --sparseGRMFile=/public/home/zyli/UKB_WES_3rd/GRM/UKB_GRM_relatednessCutoff_0.05_5000_randomMarkersUsed_unrelated_3rd_relatednessCutoff_0.05_5000_randomMarkersUsed.sparseGRM.mtx \
+    --sparseGRMSampleIDFile=/public/home/zyli/UKB_WES_3rd/GRM/UKB_GRM_relatednessCutoff_0.05_5000_randomMarkersUsed_unrelated_3rd_relatednessCutoff_0.05_5000_randomMarkersUsed.sparseGRM.mtx.sampleIDs.txt \
+    --plinkFile=/public/home/zyli/UKB_WES_3rd/unrelated_0_0442/ukb_wes_chr${i}_sample_qc_final_unrelated \
+    --useSparseGRMtoFitNULL=FALSE   \
+    --useSparseGRMforVarRatio=TRUE \
+    --phenoFile=/public/home/zyli/UKB_WES_3rd/PHQ4.csv \
+    --phenoCol=PHQ4 \
+    --covarColList=sex,age,PC1,PC2,PC3,PC4,PC5,PC6,PC7,PC8,PC9,PC10 \
+    --qCovarColList=sex  \
+    --sampleIDColinphenoFile=eid \
+    --SampleIDIncludeFile=/public/home/zyli/UKB_WES_3rd/british_white_id.txt \
+    --isCovariateOffset=FALSE \
+    --traitType=quantitative  \
+    --invNormalize=TRUE \
+    --isCateVarianceRatio=TRUE \
+    --outputPrefix=/public/home/zyli/Depression_WES/PHQ4/step1/PHQ4_3rd_10PC_chr${i} \
+    --IsOverwriteVarianceRatioFile=TRUE
+
+  Rscript /public/home/zyli/UKB_WES_3rd/SAIGE_Rfile/step2_SPAtests.R \
+    --bedFile=/public/home/zyli/UKB_WES_3rd/unrelated_0_0442/ukb_wes_chr${i}_sample_qc_final_unrelated.bed \
+    --bimFile=/public/home/zyli/UKB_WES_3rd/unrelated_0_0442/ukb_wes_chr${i}_sample_qc_final_unrelated.bim \
+    --famFile=/public/home/zyli/UKB_WES_3rd/unrelated_0_0442/ukb_wes_chr${i}_sample_qc_final_unrelated.fam \
+    --SAIGEOutputFile=/public/home/zyli/Depression_WES/PHQ4/step2_single/PHQ4_3rd_10PC_chr${i}.txt \
+    --SampleIDIncludeFile=/public/home/zyli/UKB_WES_3rd/british_white_id.txt \
+    --AlleleOrder=alt-first \
+    --minMAF=0 \
+    --minMAC=20 \
+    --GMMATmodelFile=/public/home/zyli/Depression_WES/PHQ4/step1/PHQ4_3rd_10PC_chr${i}.rda \
+    --varianceRatioFile=/public/home/zyli/Depression_WES/PHQ4/step1/PHQ4_3rd_10PC_chr${i}.varianceRatio.txt \
+    --sparseGRMFile=/public/home/zyli/UKB_WES_3rd/GRM/UKB_GRM_relatednessCutoff_0.05_5000_randomMarkersUsed_unrelated_3rd_relatednessCutoff_0.05_5000_randomMarkersUsed.sparseGRM.mtx \
+  	--sparseGRMSampleIDFile=/public/home/zyli/UKB_WES_3rd/GRM/UKB_GRM_relatednessCutoff_0.05_5000_randomMarkersUsed_unrelated_3rd_relatednessCutoff_0.05_5000_randomMarkersUsed.sparseGRM.mtx.sampleIDs.txt \
+  	--is_output_moreDetails=TRUE \
+    --LOCO=FALSE \
+    --is_fastTest=FALSE
+done
+
